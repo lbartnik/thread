1833ada9c19bad6f4084fa3af40e714f055b4349
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-12-11 22:43:26 -0800

    add a threaded native call test

--- tests/testthat/test-native-call.R
+++ tests/testthat/test-native-call.R
@@ -1,6 +1,19 @@
 context("native call")
 
 test_that("native function can be called", {
-  ret <- native_call("C_sample_call", 1)
-  expect_equal(ret, 1)
+  ret <- native_call("C_sample_call", 42)
+  expect_equal(ret, 42)
+})
+
+
+test_that("native call in a separate thread", {
+  thread_runner <- function (data) {
+    native_call("C_sample_call", 42)
+  }
+  
+  th <- new_thread(thread_runner, list())
+  thread_join(th)
+  rc <- thread_result(th)
+  
+  expect_equal(rc, 42)
 })
